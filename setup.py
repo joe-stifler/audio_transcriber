49914652e29df2153a920a50215256506f5cbe77
joe-stifler <joseribeiro1017@gmail.com>
2024-04-02 18:48:23 +0100

    chore: format code

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='audio_transcriber',
-    version='0.1.0',
-    author='Joe',
-    author_email='joseribeiro1017@gmail.com',
+    name="audio_transcriber",
+    version="0.1.0",
+    author="Joe",
+    author_email="joseribeiro1017@gmail.com",
     packages=find_packages(),
     install_requires=[
-        'openai-whisper',  # Add other dependencies as needed
+        "openai-whisper",  # Add other dependencies as needed
     ],
     entry_points={
-        'console_scripts': [
-            'audio_transcriber=audio_transcriber.transcriber:main',
+        "console_scripts": [
+            "audio_transcriber=audio_transcriber.transcriber:main",
         ],
     },
-    description='A simple audio/video transcriber tool.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    description="A simple audio/video transcriber tool.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
 )
